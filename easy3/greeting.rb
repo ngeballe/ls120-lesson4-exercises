8549a1499708c6898ea350d0f934408d845e8fee
Nick Geballe <ngeballe@gmail.com>
2016-11-28 12:48:44 -0500

    Do Easy 3 exercises

---
+++ easy3/greeting.rb
@@ -0,0 +1,31 @@
+class Greeting
+  def greet(message)
+    puts message
+  end
+end
+
+class Hello < Greeting
+  def hi
+    greet("Hello")
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet("Goodbye")
+  end
+end
+
+# hello = Hello.new
+# hello.hi
+
+# hello = Hello.new
+# hello.bye
+
+# hello = Hello.new
+# hello.greet
+
+# hello = Hello.new
+# hello.greet("Goodbye")
+
+Hello.hi
